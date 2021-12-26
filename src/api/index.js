e60bd66eae26e71e00d0ba7e29f1c8c4afa1ac0b
zaar625 <zaar625@naver.com>
2021-12-27 03:07:29 +0900

    학습노트 데이터 조회 기능 구현

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,34 @@
 import axios from 'axios';
-import store from '../store';
+// import store from '../store';
 import { setInterceptors } from './common/interceptors';
 
+//엑시오스 초기화 함수
 function createInstance() {
   const instance = axios.create({
     baseURL: 'http://localhost:3000/',
-    headers: {
-      Authorization: store.state.token,
-    },
+    // headers: {
+    //   Authorization: store.state.token,
+    // },
   });
   return setInterceptors(instance);
 }
 const instance = createInstance();
 
+//회원가입 API
 function registerUser(userData) {
   // const url = 'http://localhost:3000/signup';
   //  axios.post(url, userData);
   return instance.post('signup', userData);
 }
 
+//로그인 API
 function loginUser(userData) {
   return instance.post('login', userData);
 }
-export { registerUser, loginUser };
+
+//학습 노트 데이터를 조회하는 API
+
+function fetchPosts() {
+  return instance.get('posts');
+}
+export { registerUser, loginUser, fetchPosts };
