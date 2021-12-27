3315d3cb6ba1cfb990a86e8bfe19040d02c87acc
zaar625 <zaar625@naver.com>
2021-12-27 15:52:05 +0900

    데이터 삭제 기능 구현

--- src/api/post.js
+++ src/api/post.js
@@ -1,8 +1,7 @@
 // 학습 노트 조작과 관련된 CRUD API 함수 파일
 import { posts } from './index';
 
-//학습 노트 데이터를 조회하는 API
-
+// 학습 노트 데이터를 조회하는 API
 function fetchPosts() {
   return posts.get('/');
 }
@@ -11,4 +10,10 @@ function fetchPosts() {
 function createPost(postData) {
   return posts.post('/', postData);
 }
-export { fetchPosts, createPost };
+
+// 학습 노트 데이터를 삭제하는 API
+function deletePost(postId) {
+  return posts.delete(postId);
+}
+
+export { fetchPosts, createPost, deletePost };
